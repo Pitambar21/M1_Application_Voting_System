7f97a6eca0233b79a2281924704c611a1c10a670
Pitambar21 <94235986+Pitambar21@users.noreply.github.com>
2021-11-23 13:07:24 +0530

    vote.h

---
+++ 3_Implementation/inc/vote.h
@@ -0,0 +1,27 @@
+/**
+ * @file vote.h
+ * @author Pitambar (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+   
+/ Header files 
+
+#include <stdio.h>
+#define CD_COUNT
+#define CD1 "PARTY_A"
+#define CD2 "PARTY_B"
+#define CD3 "PARTY_C"
+#define CD4 "PARTY_D"
+
+/ void for voting system
+
+void castVote()
+void castVote()
+void getLeadingCD()
+
+#endif  /* __Your vote has been recorded__*/
